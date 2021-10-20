df4cecad31c622629a1cf8ba8931ecc77d9231d9
Xavier Pun <65343237+X3vvv@users.noreply.github.com>
2021-10-20 19:53:47 +0800

    adjust format and add comments

--- js_helpers/otherHelper.js
+++ js_helpers/otherHelper.js
@@ -1,4 +1,4 @@
-function markForDeletion(array,index) {
+function markForDeletion(array, index) {
   array[index] = undefined;
 }
 
@@ -10,34 +10,34 @@ function deleteMarked(array) {
       newIndex += 1;
     }
   }
-  return array.slice(0,newIndex);
+  return array.slice(0, newIndex);
 }
 
 
 // hsv to rgb
 function HSVtoRGB(h, s, v) {
-    var r, g, b, i, f, p, q, t;
-    if (h && s === undefined && v === undefined) {
-        s = h.s, v = h.v, h = h.h;
-    }
-    i = Math.floor(h * 6);
-    f = h * 6 - i;
-    p = v * (1 - s);
-    q = v * (1 - f * s);
-    t = v * (1 - (1 - f) * s);
-    switch (i % 6) {
-        case 0: r = v, g = t, b = p; break;
-        case 1: r = q, g = v, b = p; break;
-        case 2: r = p, g = v, b = t; break;
-        case 3: r = p, g = q, b = v; break;
-        case 4: r = t, g = p, b = v; break;
-        case 5: r = v, g = p, b = q; break;
-    }
-    return {
-        r: r,
-        g: g,
-        b: b
-    };
+  var r, g, b, i, f, p, q, t;
+  if (h && s === undefined && v === undefined) {
+    s = h.s, v = h.v, h = h.h;
+  }
+  i = Math.floor(h * 6);
+  f = h * 6 - i;
+  p = v * (1 - s);
+  q = v * (1 - f * s);
+  t = v * (1 - (1 - f) * s);
+  switch (i % 6) {
+    case 0: r = v, g = t, b = p; break;
+    case 1: r = q, g = v, b = p; break;
+    case 2: r = p, g = v, b = t; break;
+    case 3: r = p, g = q, b = v; break;
+    case 4: r = t, g = p, b = v; break;
+    case 5: r = v, g = p, b = q; break;
+  }
+  return {
+    r: r,
+    g: g,
+    b: b
+  };
 }
 
 function convertHue(hue) {
@@ -48,10 +48,28 @@ function convertHue(hue) {
 }
 
 function componentToHex(c) {
-    var hex = Math.round(c*255).toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
+  var hex = Math.round(c * 255).toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
 }
 
 function rgbToHex(c) {
-    return "#" + componentToHex(c.r) + componentToHex(c.g) + componentToHex(c.b);
+  return "#" + componentToHex(c.r) + componentToHex(c.g) + componentToHex(c.b);
 }
+
+// function time() {
+//   // return Unix timestamp, milliseconds
+//   return new Date().getTime();
+// }
+
+/**
+ * Compatible way of implement requesAnimFrame.
+ * Code from: https://www.cnblogs.com/zhangycun/p/7809633.html
+ */
+// window.requestAnimFrame = (function () {
+//   return window.requestAnimationFrame ||
+//     window.webkitRequestAnimationFrame ||
+//     window.mozRequestAnimationFrame ||
+//     function (callback) {
+//       window.setTimeout(callback, 6000 / 60);
+//     };
+// })();
